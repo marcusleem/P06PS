48ecb43566193ac0329baba83565af5da04e7317
marcusleem <Thengtheng5>
2020-05-29 16:48:37 +0800

    final

--- app/src/main/java/com/android/p06ps/MainActivity.java
+++ app/src/main/java/com/android/p06ps/MainActivity.java
@@ -2,13 +2,41 @@ package com.android.p06ps;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnAdd;
+    ListView lv;
+    ArrayAdapter aa;
+    ArrayList<Task> al;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnAdd = findViewById(R.id.btnAdd);
+        lv = findViewById(R.id.lv);
+
+        DBHelper dbh = new DBHelper(MainActivity.this);
+        al = dbh.getAllTask();
+        dbh.close();
+
+        aa = new ListAdapter(this, R.layout.row, al);
+        lv.setAdapter(aa);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, AddActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
